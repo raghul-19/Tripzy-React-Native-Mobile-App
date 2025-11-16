fce8a0991b83bab1ea4ff1524c7e6cc2dd2e89a3
CipherAlgo-19 <cipheralgo19@gmail.com>
2025-11-16 13:10:47 +0530

    app json is configured as app.config.json

---
+++ Frontend/app.config.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+
+export default ({ config }) => ({
+  ...config,
+  name: "Tripzy",
+  slug: "tripzy",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "tripzy",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    config: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    },
+  },
+  androidNavigationBar: {
+    backgroundColor: "#ffffff",
+    barStyle: "dark-content",
+  },
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "cover",
+    backgroundColor: "#2F80ED",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    package: "com.anonymous.tripzy",
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+    bundler: "metro",
+  },
+  plugins: [
+    "expo-router",
+    "./plugins/withAndroidTransparentSystemBars.js",
+    "expo-web-browser",
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "b5f4ef60-c9fc-47de-8664-4520cc2914a1",
+    },
+  },
+});
+
